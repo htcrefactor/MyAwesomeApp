2f9f65ea881edc111b6cc1f8acab18ab59ea8ad0
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-28 12:23:55 +0900

    11. Give width and align styles.

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ class CounterButton extends React.Component {
   
   render() {
     return (
-      <View>
+      <View style = {styles.buttonContainer}>
         <Text>Current Count: {this.state.counter}</Text>
         <Button title = {"Click Me!"} onPress = {this.clickHandler} />
       </View>
@@ -50,5 +50,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  buttonContainer: {
+    alignItems: 'center',
+    width: 300
   }
 });
