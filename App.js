f3d142316b938a1b0d714fee1e3db1f85ffe20a9
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-28 12:21:14 +0900

    10. Give props a default value.

--- App.js
+++ App.js
@@ -3,6 +3,10 @@ import React from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 
 class CounterButton extends React.Component {
+  static defaultProps = {
+    counter: -1
+  };
+
   constructor(props) {
     super(props);
 
@@ -32,7 +36,7 @@ export default class App extends React.Component {
     return (
       <View style = {styles.container}>
         <CounterButton counter={0} />
-        <CounterButton counter={1} />
+        <CounterButton />
         <CounterButton counter={2} />
         <CounterButton counter={3} />
       </View>
