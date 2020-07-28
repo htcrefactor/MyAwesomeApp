e1705cbd98d0d3056d61f670d623c63a9d71ac4c
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-28 17:41:03 +0900

    19. Apply ScrollView to enable scrolling overflowed content.

--- App.js
+++ App.js
@@ -1,66 +1,64 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, View, Text } from 'react-native';
+import { Image, StyleSheet,  ScrollView, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default class App extends React.Component {
   render() {
     return (
-      <SafeAreaView style={styles.fullscreen}>
-        <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }} />
+      <SafeAreaView style={styles.container}>
+        <ScrollView style={styles.container}>
+          <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
 
-        <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }} />
+          <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
 
-        <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }} />
+          <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
 
-        <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }} />
+          <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
 
-        <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }} />
+          <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
 
-        <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
-        <Image source={{
-          uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
-          width: 128,
-          height: 128,
-        }} />
-
+          <Text style={{ fontSize: 96 }}>Scroll Me Please</Text>
+          <Image source={{
+            uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1024px-React-icon.svg.png",
+            width: 128,
+            height: 128,
+          }} />
+        </ScrollView>
         <StatusBar style="auto" />
+
       </SafeAreaView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  fullscreen: {
-    flex: 1,
-    backgroundColor: '#fff',
-    borderColor: 'red',
-    borderWidth: 2,
-    alignItems: 'center'
+  container: {
+    flex: 1
   }
 });
