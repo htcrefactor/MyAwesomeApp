2f2416febd61e07fcbe7577859c78191add08a68
Hyung-Taik Choi <htc.refactor@gmail.com>
2020-07-29 13:23:23 +0900

    30. Implement react-navigation/stack.

--- App.js
+++ App.js
@@ -1,35 +1,38 @@
-import React from 'react';
+import * as React from 'react';
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import CityList from './CityList';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <SafeAreaView style={styles.container}>
-        <CityList />
-        <StatusBar style="auto" />
-      </SafeAreaView>
-    );
-  }
-}
+const HomeScreen = () => (
+  <View style={styles.container}>
+    <CityList />
+    <StatusBar style="auto" />
+  </View>
+);
+
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-
-  item: {
-    flex: 1,
-    height: 50,
-    justifyContent: 'center',
-    borderWidth: 1,
-    borderColor: 'orange'
-  },
-
-  text: {
-    fontSize: 20,
-    textAlign: 'center'
   }
 });
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options = {{ title: 'Cities'}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
